32befe1651ce6bab6afa345ee8da7ca943b33a03
Ben-Long50 <benjlong50@gmail.com>
2024-03-06 00:14:48 -0800

    began setting up the js framework

---
+++ drop-down-menu/src/index.js
@@ -0,0 +1,11 @@
+import './styles/main.css';
+import './styles/reset-css.css';
+
+const navBarItems = [];
+
+class NavBarItem {
+  constructor(name) {
+    this.name = name;
+    this.menuOptions = [];
+  }
+}
