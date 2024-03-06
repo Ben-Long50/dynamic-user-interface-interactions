935595a6c9d248d1559f137c3aab29fe0cf4a85f
Ben-Long50 <benjlong50@gmail.com>
2024-03-06 00:23:17 -0800

    further developed js

--- drop-down-menu/dist/bundle.js
+++ drop-down-menu/dist/bundle.js
@@ -787,8 +787,23 @@ var __webpack_exports__ = {};
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/main.css */ "./src/styles/main.css");
 /* harmony import */ var _styles_reset_css_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/reset-css.css */ "./src/styles/reset-css.css");
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : String(i); }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 
+var navBarItems = [];
+var NavBarItem = /*#__PURE__*/_createClass(function NavBarItem(name, menuOptions) {
+  _classCallCheck(this, NavBarItem);
+  this.name = name;
+  this.menuOptions = menuOptions;
+  navBarItems.push(this);
+}); // eslint-disable-next-line no-new
+new NavBarItem('AR-15', ['Upper Receiver', 'Lower Receiver', 'Barrel', 'Bolt Carrier Group (BCG)', 'Charging Handle', 'Handguard', 'Stock', 'Pistol Grip', 'Trigger Group', 'Magazine']);
+console.log(navBarItems);
 })();
 
 /******/ })()
--- drop-down-menu/src/index.js
+++ drop-down-menu/src/index.js
@@ -4,8 +4,25 @@ import './styles/reset-css.css';
 const navBarItems = [];
 
 class NavBarItem {
-  constructor(name) {
+  constructor(name, menuOptions) {
     this.name = name;
-    this.menuOptions = [];
+    this.menuOptions = menuOptions;
+    navBarItems.push(this);
   }
 }
+
+// eslint-disable-next-line no-new
+new NavBarItem('AR-15', [
+  'Upper Receiver',
+  'Lower Receiver',
+  'Barrel',
+  'Bolt Carrier Group (BCG)',
+  'Charging Handle',
+  'Handguard',
+  'Stock',
+  'Pistol Grip',
+  'Trigger Group',
+  'Magazine',
+]);
+
+console.log(navBarItems);
